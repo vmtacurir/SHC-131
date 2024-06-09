42acd5b0293aa1c8426a4baca7b99bf2635b4177
vmtacurir <victorcitomanuelito@hotmail.com>
2024-06-09 17:53:44 -0400

    PRIMER COMMIT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { FieldsModule } from './fields/fields.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { SchedulesModule } from './schedules/schedules.module';
+import { SchedulesController } from './schedule/controllers/schedules.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UsersModule, FieldsModule, ReservationsModule, SchedulesModule],
+  controllers: [AppController, SchedulesController],
   providers: [AppService],
 })
 export class AppModule {}
